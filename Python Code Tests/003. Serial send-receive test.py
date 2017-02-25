32b0761b66c3d5d0cdb42b4ca6c36446d2d5bc80
ovidiuhomoc <ovidiu.homoc@gmail.com>
2017-02-25 18:13:34 +0200

    Succeeded in transmiting and receiving serial communication to PC via serial port

--- Python Code Tests/003. Serial send-receive test.py
+++ Python Code Tests/003. Serial send-receive test.py
@@ -2,6 +2,7 @@ import serial
 import threading
 from gpiozero import LED,Button
 import time,datetime
+import codecs
 
 def read_serial(port):
     print ("Started serial listening")
@@ -11,27 +12,41 @@ def read_serial(port):
 
         while flag==0:
             ch=port.read()
-            rv+=ch
+            if (ch<>'\r') and (ch<>'\n'):
+                rv+=ch
+            
             if (ch=='\r') or (ch=='\n'):
-                print ("Got from serial below string \n%s"%(cv.decode('utf-8')))
+                print ("Got from serial below string \n{}".format(rv))
                 flag=1
 
 def write_serial(i):
     try:
-        ser.write("s-a apasat butonul {} la ora {}".format(i.pin.number,datetime.datetime.now()))
+        ser.write("s-a apasat butonul {} la ora {}\n\r".format(i.pin.number,datetime.datetime.now()))
+        print ("__s-a apasat butonul {} la ora {}".format(i.pin.number,datetime.datetime.now()))
     except:
         pass
 
 In1=Button(18)
 flag=0
 
-ser=serial.Serial("/dev/ttyAMA0",baudrate=115200,timeout=0)
 try:
-    ser.open()
+    ser=serial.Serial(
+        port="/dev/ttyS0",
+        baudrate=9600,
+        bytesize=serial.EIGHTBITS,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE
+    )
+    
+    if (ser.isOpen()):
+        print("Portul este deschis")
+    else:
+        print("Portul nu este deschis")
     flag =1
 except Exception as inst:
     print ("Nu s-a putut deschide portul iar exceptia este :{}".format(inst))           # __str__ allows args to be printed directly
 
 if flag:
-    threading (read_serial,[ser]).start()
+    t=threading.Thread(target=read_serial,args=[ser])
+    t.start()
     In1.when_pressed=write_serial
