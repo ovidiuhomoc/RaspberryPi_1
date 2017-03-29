cffd29f06f8127311a3f97e264ed1dfaadc10408
ovidiuhomoc <ovidiu.homoc@gmail.com>
2017-03-30 00:33:01 +0300

    Sending serial commands over RS485 | still do not receive answer | wrong RS-485 circuit

---
+++ Python Code Tests/004. Send Serial command to arduino.py
@@ -0,0 +1,72 @@
+import serial
+import threading
+from gpiozero import LED,Button
+import time,datetime
+import codecs
+
+def read_serial(port):
+    print ("Started serial listening")
+    while True:
+        rv=""
+        flag=0
+
+        while flag==0:
+            ch=port.read()
+            if (ch<>'\r') and (ch<>'\n'):
+                rv+=ch
+            
+            if (ch=='\r') or (ch=='\n'):
+                print ("Got from serial below string \n{}".format(rv))
+                flag=1
+
+def button_press():
+    threading.Thread(target=write_serial,args=[1]).start()
+    threading.Timer(2,write_serial,[2]).start()
+
+def write_serial(i):
+    ctrl.on()
+    adr_exp=10
+    adr_dest=1
+    elemente=1
+    final=5
+
+    print ("s-a apasat butonul si se transmite pe serial")
+    
+    try:   
+        ser.write("{}|{}|{}|{}|{}\n\r".format(adr_exp,adr_dest,i,elemente,final))
+        print ("__{}|{}|{}|{}|{}\n\r".format(adr_exp,adr_dest,i,elemente,final))
+    except Exception as inst:
+        print ("nu s-a trecut de try iar exceptia este: {}".format(inst))    
+        pass
+    ctrl.off()
+
+In1=Button(18)
+flag=0
+ctrl=LED(16)
+ctrl.off()
+
+try:
+    ser=serial.Serial(
+        port="/dev/ttyS0",
+        baudrate=9600,
+        bytesize=serial.EIGHTBITS,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+        rtscts=0,
+        xonxoff=0,
+        dsrdtr=0,
+        timeout=None
+    )
+    
+    if (ser.isOpen()):
+        print("Portul este deschis")
+        flag =1
+    else:
+        print("Portul nu este deschis")
+except Exception as inst:
+    print ("Nu s-a putut deschide portul iar exceptia este :{}".format(inst))           # __str__ allows args to be printed directly
+
+if flag:
+    t=threading.Thread(target=read_serial,args=[ser])
+    t.start()
+    In1.when_pressed=button_press
