29927713eb4d216a129d77fbc27e612248449ba1
ovidiuhomoc <ovidiu.homoc@gmail.com>
2017-07-09 23:28:08 +0300

    buffer serial output trasnmis si catre input

--- Python Code Tests/005. Interpret Serial response.py
+++ Python Code Tests/005. Interpret Serial response.py
@@ -3,20 +3,62 @@ import threading
 from gpiozero import LED,Button
 import time,datetime
 import codecs
+import string
+from string import *
+
+portSerial=0
+serialFlag=0
+
+def decode_string(text):
+    counter=0
+    sep="|"
+    sep1=text.find(sep)
+    inByte=[]
+    
+    if (sep1!=-1):
+        y=len(text)
+        x=text[:sep1]
+        counter=counter+1
+        inByte.append(int(x))
+
+        lastIndex=text.rfind(sep)
+
+        while sep1!=lastIndex:
+            x=text[sep1+1:text.find(sep,sep1+1)]
+            counter=counter+1
+            inByte.append(int(x))
+            sep1=text.find(sep,sep1+1)
+
+        x=text[sep1+1]
+        counter=counter+1
+        inByte.append(int(x))
+
+        for i in inByte:
+            print ("\{}/".format(i))
+        print("*{}*".format(counter))
 
 def read_serial(port):
     print ("Serial ascult")
+    global serialFlag
+    oldSerialFlag=serialFlag
+    
     while True:
         rv=""
         flag=0
+        if(oldSerialFlag!=serialFlag):
+            print("serialFlag={}".format(serialFlag))
+            oldSerialFlag=serialFlag
+            time.sleep(1)
 
-        while flag==0:
+        
+        while (flag==0)&(serialFlag==0):            
             ch=port.read()
             if (ch<>'\r') and (ch<>'\n'):
                 rv+=ch
             
             if (ch=='\r') or (ch=='\n'):
                 print ("Serial recept:{}\n".format(rv))
+                #decode_string(rv)
                 flag=1
 
 def button_press():
@@ -24,25 +66,31 @@ def button_press():
     threading.Timer(2,write_serial,[2]).start()
 
 def write_serial(i):
+
+    global serialFlag
     
     adr_exp=10
     adr_dest=1
     elemente=15
     final=5
 
-    print ("Serial trimis:")
-    
     try:
+        serialFlag=1
+        print(serialFlag)
+        print("-------------------------------------")
         ser.write("{}|{}|{}|{}|{}\n\r".format(adr_exp,adr_dest,i,elemente,final))
-        print ("{}|{}|{}|{}|{}".format(adr_exp,adr_dest,i,elemente,final))
+        print ("Serial trimis:{}|{}|{}|{}|{}\n".format(adr_exp,adr_dest,i,elemente,final))
+        serialFlag=0
+        print(serialFlag)
     except Exception as inst:
-        print ("nu s-a trecut de try iar exceptia este: {}".format(inst))    
+        print ("Eroare transmisiune seriala: {}".format(inst))
+        serialFlag=1
         pass
     
 In1=Button(18)
-portSerial=0
 
 try:
+    global ser
     ser=serial.Serial(
         port="/dev/ttyS0",
         baudrate=9600,
